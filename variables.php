f62074c03a25b975a2a7386dd50c0953765f2d21
Regina <regina.borisovaus@gmail.com>
2023-04-10 17:05:41 -0400

    resolve #16 cometted forgotten file

---
+++ variables.php
@@ -0,0 +1,25 @@
+<?php
+$h     = 'Home';
+$ab    = 'About Me';
+$mw    = 'My Work';
+$rm    = 'How to reach me';
+$email = 'E-mail';
+$phone = 'Phone';
+$address= 'Address';
+$name='Regina';
+$lname= 'Borisova';
+
+$menuitems=[
+    '/index.php'=> 'Home',
+    '/about.php'=> 'About Me',
+    '/work.php' => 'My Work',
+    '/contact.php'=>'How to reach me'
+                 ];
+  $icons=[
+      ["name" => "linkedin", "link" => "https://www.linkedin.com/in/regina-borisova-11777a24a/"],
+      ["name" => "facebook",  "link" => "https://www.facebook.com/lina.gusman.790"],
+      ["name" => "github", "link" => "https://github.com/ReginaBorisova"]
+  ];
+
+
+?>
