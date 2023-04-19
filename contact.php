29ebc778c28399f862dd4754716491550884f723
Regina <regina.borisovaus@gmail.com>
2023-04-19 12:20:45 -0400

    resolve #15 Finalizing The Basic PHP Milestone

--- contact.php
+++ contact.php
@@ -2,27 +2,10 @@
 <!DOCTYPE html>
 <html lang="en">
 <?php require'./include/head.php';?>
-<!--<head>-->
-<!--    <meta charset="UTF-8">-->
-<!--    <meta http-equiv="X-UA-Compatible" content="IE=edge">-->
-<!--    <meta name="viewport" content="width=device-width, initial-scale=1.0">-->
-<!--    <link rel="stylesheet" href="/css/main.css">-->
-<!--    <link rel="stylesheet" href="/css/picture.css">-->
-<!--    <script src="https://kit.fontawesome.com/7f1d61da1e.js" crossorigin="anonymous"></script>-->
-<!--</head>-->
 <title><?=$titleContact?></title>
 <body>
    <header>
        <?php require './include/menu.php';?>
-<!--   <div class="menu-btn">-->
-<!--    <div class="btn-line"></div>-->
-<!--    <div class="btn-line"></div>-->
-<!--    <div class="btn-line"></div>-->
-<!--   </div>-->
-<!--   <nav class="menu">-->
-<!--    <div class="menu-branding">-->
-<!--     <div class="portrait"><img src="/img/WebPagePic.jpg"/></div>-->
-<!--    </div>-->
     <ul class="menu-nav">
         <?php
         foreach ($menuitems as $url => $label){
@@ -55,6 +38,5 @@
     </div>
    </main>
    <?php require'./include/footer.php';?>
-<!--   <script src="/js/main.js"></script>-->
 </body>
 </html>
