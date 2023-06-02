0beeb00ab12adbe0bc01881951a04c7e85af239c
Regina <regina.borisovaus@gmail.com>
2023-06-02 00:08:08 -0400

    #71 added function

--- functions/functions.php
+++ functions/functions.php
@@ -12,6 +12,18 @@ function getPageTitle($page_titles)
     return $title;
 }
 /**
+ * Returns a current page with id="here".
+ *
+ * @param int $url The current page.
+ *
+ * @return string with id="here" for the current page.
+ */
+function getCurrentPageId($url) {
+    $currentPage = basename($_SERVER['SCRIPT_FILENAME']);
+    $active = ($currentPage == basename($url)) ? 'id="here"' : '';
+    return $active;
+}
+/**
  * Returns a string with a title based on the given title type.
  *
  * @param int $titleType The title type.
