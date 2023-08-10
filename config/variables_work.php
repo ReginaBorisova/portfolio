f8f9dc2a6adeaebb2670d7df99a8d8dd7eac9848
Regina <regina.borisovaus@gmail.com>
2023-08-10 16:20:32 -0400

    feature/81 added place links under project name for future sites i will publish

--- config/variables_work.php
+++ config/variables_work.php
@@ -4,30 +4,35 @@ $work = 'Work';
 $title = 'Middle Web Dev';
 $checkMyProjects = 'Check out some of my projects';
 $projects = [
-[
-'img' => '/img/projects/project1.jpg',
-'project' => 'Color Schemes',
-'github' => 'https://github.com/ReginaBorisova/Color-Schemes-Practice'
-],
-[
-'img' => '/img/projects/project2.jpg',
-'project' => 'Portfolio',
-'github' => 'https://github.com/ReginaBorisova/portfolio'
-],
-[
-'img' => '/img/projects/project3.jpg',
-'project' => 'TLF Draft',
-'github' => 'https://github.com/ReginaBorisova/Lucky-Foundation-Draft'
-],
-[
-'img' => '/img/projects/project4.jpg',
-'project' => 'Project 4',
-'github' => 'Github'
-],
-[
-'img' => '/img/projects/project5.jpg',
-'project' => 'Project 5',
-'github' => 'Github'
-]
+    [
+        'img' => '/img/projects/project1.jpg',
+        'project' => 'Color Schemes',
+        'github' => 'https://github.com/ReginaBorisova/Color-Schemes-Practice',
+        'name_link' => '#'
+    ],
+    [
+        'img' => '/img/projects/project2.jpg',
+        'project' => 'Portfolio',
+        'github' => 'https://github.com/ReginaBorisova/portfolio',
+        'name_link' => '#'
+    ],
+    [
+        'img' => '/img/projects/project3.jpg',
+        'project' => 'TLF Draft',
+        'github' => 'https://github.com/ReginaBorisova/Lucky-Foundation-Draft',
+        'name_link' => '#'
+    ],
+    [
+        'img' => '/img/projects/project4.jpg',
+        'project' => 'Project 4',
+        'github' => 'Github',
+        'name_link' => '#'
+    ],
+    [
+        'img' => '/img/projects/project5.jpg',
+        'project' => 'Project 5',
+        'github' => 'Github',
+        'name_link' => '#'
+    ]
 ];
 ?>
